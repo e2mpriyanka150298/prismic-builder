3e9205d3213651394cca485a6f4934a7def4f496
e2mpriyanka150298 <priyanka.thakkar@e2m.solutions>
2024-06-20 20:01:41 +0530

    initial push

---
+++ app/src/components/footer.tsx
@@ -0,0 +1,23 @@
+import { createClient  } from "@/prismicio";
+import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
+
+export default async function Footer() {
+
+  const client = createClient();
+  const settings = await client.getSingle('settings');
+
+  return (
+    <footer className="">
+      <Link href="/">{settings.data.site_title}</Link>
+      <p>©{ new Date().getFullYear() } {settings.data.site_title}</p>
+      <nav>
+        <ul>
+          {settings.data.navigation.map(({label, link}) => (
+            <li><PrismicNextLink field={link}>{label}</PrismicNextLink></li>
+          ))}
+        </ul>
+      </nav>
+    </footer>
+  )
+}
